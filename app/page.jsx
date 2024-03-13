941a025e47a7b1afd128fcd9716f9ba2e649a1ae
Dave Dove <davedove@icloud.com>
2024-03-13 08:18:17 +0000

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+	title: 'Home | Jet & Ben | Durham Constabulary',
+	description:
+		'Durham Police’s Safety Dogs, we visit educational settings and other community engagement events.',
+	keywords:
+		'Durham Police, Safety Dogs, Educational, Community, Engagement, Events',
+	// image: '/images/preview.png',
+	url: 'https://jetandben.com,',
+};
+
+const HomePage = () => {
+	return <div>HomePage</div>;
+};
+
+export default HomePage;
