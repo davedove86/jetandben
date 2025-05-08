ba82d6b1f8395033d21e12c13782fde8fd943d52
Dave Dove <info@dovedesign.io>
2025-05-08 13:54:32 +0100

    404 page added

---
+++ src/routes/+error.svelte
@@ -0,0 +1,49 @@
+<section>
+  <div class="container">
+    <div class="flex-items">
+      <img src="/jetandben-logo.svg" alt="Jet & Ben Logo">
+      <div class="error-content">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <p>Sorry, we couldn't find the page you were looking for.</p>
+        <a href="/" class="btn">Go to Home</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+  .flex-items {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  img {
+    max-width: 100px;
+  }
+
+  .error-content {
+    margin-top: 2rem;
+  }
+
+  h1 {
+    font-size: 6rem;
+    font-weight: bold;
+  }
+
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.2rem;
+    margin-bottom: 1.5rem;
+  }
+
+</style>
+
+
